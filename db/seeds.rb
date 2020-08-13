b1b6535f9d0c4715c99630a95342cc0e06a02e52
Steffen Staengle <steffen.staengle@gmail.com>
2020-08-13 16:56:09 +0200

    Prepare rails app with external specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# ingredient_1 = Ingredient.create(name: "lemon")
+# ingredient_2 = Ingredient.create(name: "ice")
+# ingredient_3 = Ingredient.create(name: "mint leaves")
+
+
+require 'faker'
+Ingredient.destroy_all
+20.times do
+  Ingredient.create(
+    name: Faker::Food.ingredient)
+end
+
+puts "Created #{Ingredient.count} Ingredients"
